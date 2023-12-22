cb91e85bc97c5df9bcb6ab4faa368bdb626ee905
Altobert <alberto.sanmartin@usach.cl>
2023-12-21 22:39:43 -0300

    documentacion y codigo referente a funcionalidd

--- implementacion_gruaCarga.c
+++ implementacion_gruaCarga.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include "tda_gruaCarga.h"
 
-int embarcar(int probabilidad)
-{
-    return 0;
+// La funcion debe retornar, con cierta probabilidad, un 1 si se puede embarcar o un 0 si no se puede embarcar
+int embarcar(int probabilidad){
+    int embarcar = 0;
+    int probabilidad = 0;
+    int probabilidad = rand() % 100;
+    if (probabilidad <= 50){
+        embarcar = 1;
+    }
+    else{
+        embarcar = 0;
+    }
+    return embarcar;
 }
--- tda_gruaCarga.h
+++ tda_gruaCarga.h
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//estructura de datos para registrar los datos de la grua
-typedef struct gruaCarga{
-    int id;
-    char *nombre;
-    int capacidad;
-    int estado; // 0: disponible, 1: ocupado
-}gruaCarga;
-
-
 //estructura para simular cola de embarques
 //representacion cola de embarques: (c 1 , c 2 , c 3 , . . . , c n ).
 typedef struct colaEmbarques{
-    int identificador; //numero > 0 indica orden de llegada a la cola
-    
+        
     //(0, X, 2X,3X, . . . )
-    int instanteLlegada; //instante de llegada a la cola > a cero o menor igual que el valor que el tiempo de la simulacion
-    int idBarco;
-    int peso;
-    int estado; // 0: pendiente, 1: realizado
+    //X: tiempo de embarque de una carga
+    //0: tiempo de embarque de una carga
+    //2X: tiempo de embarque de una carga
+    //3X: tiempo de embarque de una carga
+    // . . . : tiempo de embarque de una carga
+    int identificador;   //numero > 0 indica orden de llegada a la cola
+    int instanteLlegada; //instante de llegada a la cola > a cero o menor igual que el valor que el tiempo de la simulacion        
+    int estado;          // 0: pendiente, 1: realizado
+    struct colaEmbarques *siguiente;
 }colaEmbarques;
 
 /*
@@ -35,3 +30,31 @@ ese instante, asumiendo un tiempo de embarque de una unidad de tiempo por cada
 carga. En caso de que durante un perı́odo de embarque arribe una nueva carga a la
 cola, esta no será embarcada, teniendo que esperar en la cola hasta que se active el siguiente perı́odo de embarque.
 */
+
+/*
+Se le solicita a usted la construcción de un programa en lenguaje C que simule el
+comportamiento de una cola de embarque. Para su programa considere:
+*/
+
+
+//funcion que retorna un numero aleatorio entre 0 y 1
+bool embarcar(int probabilidad);
+
+/**
+ * @brief Inserta un elemento en la cola de embarques.
+ * 
+ * @param cola La cola de embarques.
+ * @param identificador El identificador del elemento a insertar.
+ * @param instanteLlegada El instante de llegada del elemento a insertar.
+ * 
+  */
+/**
+ * @brief Crea una cola de embarques.
+ * 
+ * @param n El tamaño de la cola de embarques.
+ * @return Un puntero a la cola de embarques creada.
+ */
+colaEmbarques * crearColaEmbarques(int n);
+
+
+
